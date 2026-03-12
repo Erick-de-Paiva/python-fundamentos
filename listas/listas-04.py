4f4aa84220a3f4f5eecb410818ded3ee539ab92c
Erick <paiva.erick2001@gmail.com>
2026-03-12 14:12:44 -0300

    listas04

---
+++ listas/listas-04.py
@@ -0,0 +1,30 @@
+# Crie um programa que vai ler vários números e colocar em uma lista. Depois disso, mostre:
+# A) Quantos números foram digitados.
+# B) A lista de valores, ordenada de forma decrescente.
+# C) Se o valor 5 foi digitado e está ou não na lista.
+
+lista = []
+while True:
+    n = int(input('Digite um valor: '))
+    lista.append(n)
+    escolha = str(input('Quer continuar? [S/N] ')).strip().upper()
+    while escolha != 'S' and escolha != 'N':
+        escolha = str(input('Opção inválida! Quer continuar? [S/N] ')).strip().upper()
+    if escolha == 'N':
+        break
+print(f'Você digitou {len(lista)} valores.')
+lista.sort(reverse=True)
+print(f'Os valores em ordem decrescente são: ',end='')
+for i in range(0,len(lista)):
+    print(f'{lista[i]}',end=' ')
+print()
+cincoLista = ''
+for i, j in enumerate(lista):
+    if j == 5:
+        cincoLista = 'S'
+    else:
+        cincoLista = 'N'
+if cincoLista == 'S':
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não faz parte da lista!')
